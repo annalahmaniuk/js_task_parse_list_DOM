baf66ca480931262dc1e496dc55c6cef9abeab17
Hanna <anutalahmaniuk@mail.com>
2025-09-02 15:23:34 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,9 +26,13 @@ function parseSalary(salaryStr) {
 }
 
 function getEmployees(list) {
-  const selector = Array.from(list.querySelectorAll('li'));
-
-  return selector.map((li) => ({
-    name: li.textContent.trim(),
+  return Array.from(list.querySelectorAll('li')).map((li) => ({
+    name: li.dataset.name || li.querySelector('.name')?.textContent.trim(),
+    position:
+      li.dataset.position || li.querySelector('.position')?.textContent.trim(),
+    salary: parseSalary(
+      li.dataset.salary || li.querySelector('.salary')?.textContent,
+    ),
+    age: Number(li.dataset.age || li.querySelector('.age')?.textContent),
   }));
 }
